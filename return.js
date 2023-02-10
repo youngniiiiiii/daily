949bab94f27485a2afa9b6179757986b4d17576a
youngniiiiiii <youngnikk4929@gmail.com>
2023-02-10 17:29:07 +0900

    function

---
+++ return.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  //console.log ( a + b );
+  //return a + b;  //저장해서 반환
+}
+
+var result = add(3, 4);
+console.log("두 수를 더한 값 : " + result);
+
+const a = 100;
+function sample(a) {
+  if (a > 0) {
+    return;
+  }
+  console.log("hello world");
+}
+
+sample();
